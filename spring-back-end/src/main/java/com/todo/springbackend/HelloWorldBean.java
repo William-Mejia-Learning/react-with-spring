e595e400f291bde9a33c8dc2ec4fe4c64eaa5aef
William Mejia <willx1995@gmail.com>
2022-07-04 18:28:47 -0500

    adding the spring boot server, currently on section6 step 39

---
+++ spring-back-end/src/main/java/com/todo/springbackend/HelloWorldBean.java
@@ -0,0 +1,18 @@
+package com.todo.springbackend;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
