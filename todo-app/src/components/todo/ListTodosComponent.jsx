838c80a07f07a529d5c58d24e38c2ab5b213fe1a
William Mejia <willx1995@gmail.com>
2022-07-05 18:32:54 -0500

    section 6 step 43

--- todo-app/src/components/todo/ListTodosComponent.jsx
+++ todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,30 +1,23 @@
 import React, { Component } from "react";
+import TodoDataService from "../../api/TodoDataService";
+import AuthenticationService from "./AuthenticationService";
 
 class ListTodosComponent extends Component {
     constructor(props) {
       super(props);
       this.state = {
         todos: [
-          {
-            id: 1,
-            description: "Learn React",
-            done: false,
-            targetDate: new Date(),
-          },
-          {
-            id: 2,
-            description: "Become an expert programmer",
-            done: false,
-            targetDate: new Date(),
-          },
-          {
-            id: 3,
-            description: "Visit India",
-            done: false,
-            targetDate: new Date(),
-          },
+          
         ],
       };
+    }
+
+    componentDidMount(){
+      let username = AuthenticationService.getLoggedInUsername;
+      TodoDataService.retrieveAllTodos(username).then(response => {
+        // console.log(response);
+        this.setState({todos :response.data })
+      })
     }
   
     render() {
